b45bd1e6847aeaeecb302a8c4369164419c4c377
phogya <phogya09@gmail.com>
2020-08-31 20:39:38 -0400

    Set up microservice structure with docker compose

---
+++ server/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors')
+
+var app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+let authorized = true
+
+function checkAuth(req, res, next) {
+  if (authorized) {
+    next()
+  } else {
+    res.status(403).send('Unauthorized!')
+    return
+  }
+}
+
+app.use('/', checkAuth)
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  })
+})
+
+module.exports = app;
