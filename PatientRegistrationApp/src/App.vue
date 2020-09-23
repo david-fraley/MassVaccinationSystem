810d655c13ad8749e2c39fe573d151a22d15b953
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2020-09-22 23:24:06 -0500

    [MVS-202] Create base framework for Patient registration Vue application
    Created base application using Vue and Vuetify framework

---
+++ PatientRegistrationApp/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+
+  <v-app id="Patient-registration">
+  
+    <v-main>
+		<v-container
+			class="fill-height"
+			fluid
+		>
+			<v-row
+				align="center"
+				justify="center"
+			>
+				<v-col
+					cols="12"
+				>
+					<v-card 
+						class="elevation-12" 
+						min-width="400"
+						max-width="1000"
+					>
+						<v-toolbar
+							color="primary"
+							dark
+							flat
+						>
+							<v-toolbar-title>COVID-19 Vaccination Registration</v-toolbar-title>
+						</v-toolbar>
+						
+						<v-form class ="grey lighten-5">
+							<GreetingPage/>
+						</v-form>
+						
+						<v-card-actions>
+							<v-icon
+								large
+								color="secondary"
+								@click="previous"
+							>
+								mdi-chevron-left
+							</v-icon>
+							
+							<v-spacer></v-spacer>
+							
+							<v-icon
+								large
+								color="secondary"
+								@click="next"
+							>
+								mdi-chevron-right
+							</v-icon>
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-main>
+  </v-app>
+  
+</template>
+
+<script>
+import GreetingPage from './components/GreetingPage';
+
+export default {
+	name: 'App',
+	methods: 
+	{
+		next() 
+		{
+			alert('You clicked next!')
+		},
+		previous() 
+		{
+			alert('You clicked previous!')
+		}
+	},
+  components: 
+  {
+    GreetingPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ PatientRegistrationApp/src/components/GreetingPage.vue
@@ -0,0 +1,86 @@
+
+<template>
+	<v-container fluid>
+	
+		<v-row
+			align="center"
+			justify="center"
+		>
+		
+			<v-col class="d-flex" cols="10" sm="10">
+				<v-select
+					:items="items"
+					label="Please select your preferred language"
+					dense
+				></v-select>
+			</v-col>
+		</v-row>
+     
+		<v-row
+			align="center"
+			justify="center"
+		>
+		
+			<v-col 
+				class="d-flex" 
+				cols="12" 
+				sm="6"
+			>
+				
+				<v-tooltip top>
+					<template v-slot:activator="{on, attrs}">
+						<v-btn 
+							class="ma-2"
+							outlined 
+							rounded 
+							x-large 
+							color="secondary"
+							v-bind="attrs"
+							v-on="on"
+						>
+							<v-icon left>mdi-account</v-icon>
+							Register myself
+						</v-btn>
+					</template>
+					<span>Register yourself!</span>
+				</v-tooltip>
+				
+			</v-col>
+
+			<v-col 
+				class="d-flex" 
+				cols="12" 
+				sm="6"
+			>
+				<v-tooltip top>
+					<template v-slot:activator="{on, attrs}">
+						<v-btn 
+							class="ma-2"
+							outlined 
+							rounded 
+							x-large 
+							color="secondary"
+							v-bind="attrs"
+							v-on="on"
+						>
+							<v-icon left>mdi-account-group</v-icon>
+							Register my household
+						</v-btn>
+					</template>
+					<span>Household definition...</span>
+				</v-tooltip>
+				
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'GreetingPage',
+	data: () => ({
+	items: ['English','Spanish'],
+	}),
+  }
+</script>
