bfb154f258562ac162ecc950bbaeefb828cfada0
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2020-09-28 08:25:02 -0500

    Update App.vue
    Created base navigation scheme for single patient registration (to do: update the titles per page)

--- PatientRegistrationApp/src/App.vue
+++ PatientRegistrationApp/src/App.vue
@@ -1,53 +1,247 @@
 <template>
 
   <v-app id="Patient-registration">
- 
+  
     <v-main>
-		<v-container bg fill-height grid-list-md text-xs-center>
-			<v-row
-				align="center"
-				justify="center"
+		<v-container>
+			
+			<v-card 
+				class="elevation-12"
+				min-width="400"
+				max-width="1000"
 			>
-				<v-col
-					cols="12"
+				
+				<v-stepper 
+					v-model="page"
 				>
-					<v-card 
-						class="elevation-12" 
-						min-width="400"
-						max-width="1000"
-					> 
-						<v-toolbar
-							color="blue darken-4"
-							
-						>
-						<v-toolbar-title primary-title class="grey--text text--lighten-5">COVID-19 Vaccination Registration</v-toolbar-title>
-						</v-toolbar>						
-						<v-form class ="grey lighten-5">
-							<GreetingPage/>
-						</v-form>
+				
+					<v-stepper-header>
+					
+						<!-- Greeting Page -->
+						<v-stepper-step
+							:complete="page > 1"
+							step="1"
+						></v-stepper-step>
 						
-						<v-card-actions> 
-							<v-icon
-								large
-								color="#FDD835"
-								@click="previous"
+						<v-divider></v-divider>
+						
+						<!-- Home Address -->
+						<v-stepper-step
+							:complete="page > 2"
+							step="2"
+						></v-stepper-step>
+
+						<v-divider></v-divider>
+						
+						<!-- Contact Info -->
+						<v-stepper-step
+							:complete="page > 3"
+							step="3"
+						></v-stepper-step>
+
+						<v-divider></v-divider>
+						
+						<!-- Personal Info -->
+						<v-stepper-step
+							:complete="page > 4"
+							step="4"
+						></v-stepper-step>
+
+						<v-divider></v-divider>
+						
+						<!-- Emergency Contact -->
+						<v-stepper-step
+							:complete="page > 5"
+							step="5"
+						></v-stepper-step>
+
+						<v-divider></v-divider>
+
+						<!-- Review and Submit -->
+						<v-stepper-step step="6">
+						</v-stepper-step>
+					</v-stepper-header>
+					
+					<v-toolbar
+						color="primary"
+						dark
+						flat
+					>
+					<v-toolbar-title>COVID-19 Vaccination Registration</v-toolbar-title>
+					</v-toolbar>			
+					
+					<v-stepper-items>
+						
+						<!-- Greeting Page -->
+						<v-stepper-content step="1">
+							<v-card
+								flat
+							>							
+								<GreetingPage/>
+							</v-card>
+				
+							<v-card-actions>
+																
+								<v-spacer></v-spacer>
+								
+								<v-icon
+									large
+									color="secondary"
+									@click="page=2"
+								>
+									mdi-chevron-right
+								</v-icon>
+							</v-card-actions>
+						</v-stepper-content>
+						
+						<!-- Home Address -->
+						<v-stepper-content step="2">
+							<v-card
+								flat
 							>
-								mdi-chevron-left
-							</v-icon>
+								<!-- placeholder -->
+							</v-card>
 							
-							<v-spacer></v-spacer>
-							
-							<v-icon
-								large
-								color="#FDD835"
-								@click="next"
+							<v-card-actions>
+								<v-icon
+									large
+									color="secondary"
+									@click="page=1"
+								>
+									mdi-chevron-left
+								</v-icon>
+								
+								<v-spacer></v-spacer>
+								
+								<v-icon
+									large
+									color="secondary"
+									@click="page=3"
+								>
+									mdi-chevron-right
+								</v-icon>
+							</v-card-actions>
+						</v-stepper-content>
+						
+						<!-- Contact Info -->
+						<v-stepper-content step="3">
+							<v-card
+								flat
 							>
-								mdi-chevron-right
-							</v-icon>
-						</v-card-actions>
-					</v-card>
-				</v-col>
-			</v-row>
+								<!-- placeholder -->
+							</v-card>
+							
+							<v-card-actions>
+								<v-icon
+									large
+									color="secondary"
+									@click="page=2"
+								>
+									mdi-chevron-left
+								</v-icon>
+								
+								<v-spacer></v-spacer>
+								
+								<v-icon
+									large
+									color="secondary"
+									@click="page=4"
+								>
+									mdi-chevron-right
+								</v-icon>
+							</v-card-actions>
+						</v-stepper-content>
+						
+						<!-- Personal Info -->
+						<v-stepper-content step="4">
+							<v-card
+								flat
+							>
+								<!--placeholder-->
+							</v-card>
+							
+							<v-card-actions>
+								<v-icon
+									large
+									color="secondary"
+									@click="page=3"
+								>
+									mdi-chevron-left
+								</v-icon>
+								
+								<v-spacer></v-spacer>
+								
+								<v-icon
+									large
+									color="secondary"
+									@click="page=5"
+								>
+									mdi-chevron-right
+								</v-icon>
+							</v-card-actions>
+						</v-stepper-content>
+						
+						<!-- Emergency Contact -->
+						<v-stepper-content step="5">
+							<v-card
+								flat
+							>
+								<!-- placeholder -->
+							</v-card>
+							
+							<v-card-actions>
+								<v-icon
+									large
+									color="secondary"
+									@click="page=4"
+								>
+									mdi-chevron-left
+								</v-icon>
+								
+								<v-spacer></v-spacer>
+								
+								<v-icon
+									large
+									color="secondary"
+									@click="page=6"
+								>
+									mdi-chevron-right
+								</v-icon>
+							</v-card-actions>
+						</v-stepper-content>
+						
+						<!-- Review and Submit -->
+						<v-stepper-content step="6">
+							<v-card
+								flat
+							>
+								<!-- placeholder -->
+							</v-card>
+							
+							<v-card-actions>
+								<v-icon
+									large
+									color="secondary"
+									@click="page=5"
+								>
+									mdi-chevron-left
+								</v-icon>
+								
+								<v-spacer></v-spacer>
+								
+								<v-btn
+									large
+									color="secondary"
+									@click="submit"
+								>
+									Submit
+								</v-btn>
+							</v-card-actions>
+						</v-stepper-content>
+						
+					</v-stepper-items>
+				</v-stepper>
+			</v-card>
 		</v-container>
 	</v-main>
   </v-app>
@@ -56,6 +250,7 @@
 
 <script>
 import GreetingPage from './components/GreetingPage';
+
 
 export default {
 	name: 'App',
@@ -68,15 +263,22 @@ export default {
 		previous() 
 		{
 			alert('You clicked previous!')
-		}
+		},
+		submit()
+		{
+			alert('You clicked submit!')
+		}	
 	},
   components: 
   {
-    GreetingPage,
+	GreetingPage,
   },
 
-  data: () => ({
-    //
-  }),
-};
+  data () {
+	return {
+		page: 1,
+		}
+	},
+  }
+
 </script>
