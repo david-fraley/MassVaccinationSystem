adeb61a24cc620f5bb92f4f4b45c02e422e9fe07
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2020-10-01 08:24:52 -0500

    [MVS-205] Create "Enter your home address" (single patient registration) page as a Vue component

--- PatientRegistrationApp/src/components/SinglePatientHomeAddress.vue
+++ PatientRegistrationApp/src/components/SinglePatientHomeAddress.vue
@@ -6,9 +6,101 @@
 			align="center"
 			justify="center"
 		>
-		<p>Placeholder for SinglePatientHomeAddress</p>
+		<v-row>
+		<v-col
+        cols="12"
+        sm="6"
+        md="3">
+			<v-text-field
+			:rules="['Required']"
+            label="Home Address"
+			placeholder="1234 Right Lane"
+			></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="6"
+          sm="4"
+          md="3">
+			<v-text-field
+			:rules="['Required']"
+            label="City"
+            placeholder="Hilltop"
+          ></v-text-field>
+        </v-col>
+</v-row>
+
+<v-row>
+<v-col 
+				class="d-flex" 
+				cols="4" 
+				sm="2"
+				md="3">
+							
+				<v-select
+					:rules="['Required']"
+					v-model="e6"
+                    :items="state"
+                    label="State"
+				></v-select>
+			</v-col>
+		
+<v-col 
+				class="d-flex" 
+				cols="6" 
+				sm="4">
+							
+				<v-text-field
+					:rules="['Required']"
+                    label="County"
+					placeholder="Racine County"
+				></v-text-field>
+			</v-col>
+ 
+ <v-col 
+				class="d-flex" 
+				cols="4" 
+				sm="2">
+							
+				<v-select
+					:rules="['Required']"
+					v-model="e7"
+                    :items="country"
+                    label="Country"
+				></v-select>
+			</v-col>
 			
+ <v-col
+          cols="6"
+          sm="4"
+          md="3"
+        >
+			<v-text-field
+			:rules="['Required']"
+            label="Zipcode"
+			placeholder="534444"
+			></v-text-field>
+		</v-col>
+</v-row>	
+
+		<v-row
+			align="center"
+			justify="center"
+		>
+		
+		<v-col
+			cols="12"
+			sm="6"
+			md="3"
+		>
+		
+		<v-checkbox
+			v-model="checkbox"
+			label="I have no home address"
+		></v-checkbox>
+		</v-col>	  		
 		</v-row>
+		
 	</v-container>
 </template>
 
@@ -18,6 +110,23 @@
     name: 'SinglePatientHomeAddress',
 	data () {
 		return {
+			state:[
+		'Alabama', 'Alaska', 'American Samoa', 'Arizona',
+		'Arkansas', 'California', 'Colorado', 'Connecticut',
+		'Delaware', 'District of Columbia', 'Federated States of Micronesia',
+		'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho',
+		'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+		'Louisiana', 'Maine', 'Marshall Islands', 'Maryland',
+		'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+		'Missouri', 'Montana', 'Nebraska', 'Nevada',
+		'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+		'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+		'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
+		'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+		'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
+		'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
+		],
+			country: ['United States'],
 		}
 	},
   }
