acdc99e8531dcee9bfdb233ece22f59c9d4d5b30
thomas.Ryszkiewicz <thomas.ryszkiewicz@wabbisoft.com>
2020-10-02 22:46:03 -0400

    Created emergency contact page

--- PatientRegistrationApp/src/components/SinglePatientEmergencyContact.vue
+++ PatientRegistrationApp/src/components/SinglePatientEmergencyContact.vue
@@ -1,24 +1,48 @@
 
 <template>
-	<v-container fluid>
-	
-		<v-row
-			align="center"
-			justify="center"
-		>
-		<p>Placeholder for SinglePatientEmergencyContact</p>
-			
-		</v-row>
-	</v-container>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <!-- Last name -->
+      <v-col class="d-flex" cols="5" sm="5">
+        <v-text-field
+          label="Last Name"
+          prepend-icon="mdi-menu-right"
+        ></v-text-field>
+      </v-col>
+      <!-- First name -->
+      <v-col class="d-flex" cols="5" sm="5">
+        <v-text-field label="First Name"></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <!-- Phone Number -->
+      <v-col class="d-flex" cols="5" sm="5">
+        <v-text-field
+          label="Phone Number"
+          prepend-icon="mdi-menu-right"
+        ></v-text-field>
+      </v-col>
+      <!-- Phone Number Type -->
+      <v-col class="d-flex" cols="5" sm="3">
+        <v-select
+          v-model="e7"
+          :items="phoneType"
+          label="Phone Number Type"
+        ></v-select>
+      </v-col>
+      <v-col class="d-flex" cols="5" sm="2"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
-  export default {
-    name: 'SinglePatientEmergencyContact',
-	data () {
-		return {
-		}
-	},
-  }
+export default {
+  name: "SinglePatientEmergencyContact",
+  data() {
+    return {
+      phoneType: ["Cell", "Home"],
+    };
+  },
+};
 </script>
