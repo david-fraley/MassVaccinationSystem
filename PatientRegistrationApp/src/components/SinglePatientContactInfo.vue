31f1b5368a515595d723f96d1591ef4313cb144e
grohde1015 <60712841+grohde1015@users.noreply.github.com>
2020-10-04 12:08:03 -0500

    Added components to Registration Contact info page
    Added primary and secondary phone number options with drop down's to mark type of phone; added primary and secondary email and if they can be contacted for a follow up.

--- PatientRegistrationApp/src/components/SinglePatientContactInfo.vue
+++ PatientRegistrationApp/src/components/SinglePatientContactInfo.vue
@@ -2,14 +2,127 @@
 <template>
 	<v-container fluid>
 	
-		<v-row
-			align="center"
-			justify="center"
+			<v-row>
+		<v-col
+        cols="12"
+        sm="6"
+        md="3">
+			<v-text-field
+			:rules="['Required']"
+            label="Primary Phone Number"
+			placeholder="###-###-####"
+			></v-text-field>
+        </v-col>
+
+        <v-col
+          class="d-flex" 
+				cols="6" 
+				sm="2">
+							
+				<v-select
+					:rules="['Required']"
+					v-model="e7"
+                    :items="phonetype"
+                    label="Phone Type"
+				></v-select>
+        </v-col>
+		
+		<v-col
+        cols="12"
+        sm="6"
+        md="3">
+			<v-text-field
+            label="Secondary Phone Number"
+			placeholder="###-###-####"
+			></v-text-field>
+        </v-col>
+
+        <v-col
+          class="d-flex" 
+				cols="4" 
+				sm="2">
+							
+				<v-select
+					v-model="e7"
+                    :items="phonetype"
+                    label="Phone Type"
+				></v-select>
+        </v-col>
+</v-row>
+
+<v-row>
+
+		<v-col
+			cols="6"
+			sm="6"
+			md="3"
 		>
-		<p>Placeholder for SinglePatientContactInfo</p>
-			
-		</v-row>
-	</v-container>
+		
+		<v-checkbox
+			v-model="checkbox"
+			label="I have no phone number"
+		></v-checkbox>
+		</v-col>
+
+</v-row>
+
+<v-row>
+		<v-col
+        cols="12"
+        sm="12"
+        md="6">
+			<v-text-field
+			:rules="['Required']"
+            label="Primary Email Address"
+			placeholder="johnsmith@email.com"
+			></v-text-field>
+        </v-col>
+
+		
+		<v-col
+        cols="12"
+        sm="12"
+        md="6">
+			<v-text-field
+            label="Secondary Email Address"
+			placeholder="johnsmith@email.com"
+			></v-text-field>
+        </v-col>
+
+</v-row>
+		<v-col
+			cols="6"
+			sm="6"
+			md="3"
+		>
+		
+		<v-checkbox
+			v-model="checkbox"
+			label="I have no email address"
+		></v-checkbox>
+		</v-col>
+
+
+
+<v-row>
+	<v-radio-group
+        v-model="test"
+       
+      >
+
+        <v-radio
+          label="Option 1"
+          value="radio-1"
+        ></v-radio>
+        <v-radio
+          label="Option 2"
+          value="radio-2"
+        ></v-radio>
+      </v-radio-group>
+	
+</v-row>
+
+</v-container>
 </template>
 
 <script>
@@ -17,8 +130,11 @@
   export default {
     name: 'SinglePatientContactInfo',
 	data () {
-		return {
+		return { 
+			phonetype:['Cell', 'Landline', 'Other'],
+			radios: 'May we contact you for a follow up vaccination?'
 		}
+		
 	},
   }
 </script>
