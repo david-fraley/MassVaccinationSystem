1378ccbd0dd55c1a2d8164d3e1bfd6c81811ebec
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-10-21 13:11:37 -0400

    Fix EADDRINUSE: address already in use error
    Port 3000 is in use by another microservice

--- node_app/index.js
+++ node_app/index.js
@@ -140,5 +140,5 @@ function validateCourse(course) {
 }
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Listening on port ${port}`));
