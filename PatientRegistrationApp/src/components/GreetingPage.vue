e4281af33910ef0e2f9ef15b58c9c74b445c91c8
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2020-11-08 08:29:49 -0600

    Check in initial changes

--- PatientRegistrationApp/src/components/GreetingPage.vue
+++ PatientRegistrationApp/src/components/GreetingPage.vue
@@ -1,14 +1,7 @@
 <template>
 	<v-container bg fill-height grid-list-md text-xs-center>
-		<v-row
-			align="center"
-			justify="center"
-		>
-		
-			<v-col 
-				class="d-flex" 
-				cols="9" 
-				sm="9">
+		<v-row align="center" justify="center">
+			<v-col class="d-flex" cols="10">
 				<v-select
 					:items="preferredLanguageOptions"
 					label="Please select your preferred language"
@@ -19,18 +12,9 @@
 				></v-select>
 			</v-col>
 		</v-row>
-     
-		<v-row
-			align="center"
-			justify="center"
-		>
 		
-			<v-col 
-				class="d-flex flex-wrap" 
-				cols="9" 
-				sm="9"
-			>
-				
+		<v-row align="center" justify="center">
+			<v-col class="d-flex flex-wrap" cols="10">
 				<v-btn-toggle
 					v-model="text"
 					tile
@@ -47,10 +31,14 @@
 									color="primary"
 									v-bind="attrs"
 									v-on="on"
+									height="24em"
+									width="27em"
 									@click="singleRegistration"
 							>
-									<v-icon left color="secondary">mdi-account</v-icon>
-									Register myself
+								<div>
+									<v-icon size="12em">mdi-account</v-icon>
+									<span><br /><p style="font-size:1.5em" class="font-weight-regular"><br />Register myself</p></span>
+								</div>
 							</v-btn>
 						</template>
 						<span>Register yourself!</span>
@@ -64,10 +52,14 @@
 									color="primary"
 									v-bind="attrs"
 									v-on="on"
+									height="24em"
+									width="27em"
 									@click="householdRegistration"
 								>
-									<v-icon left color="secondary">mdi-account-group</v-icon>
-									Register my household
+									<div>
+									<v-icon size="12em">mdi-account-group</v-icon>
+									<span><br /><p style="font-size:1.5em" class="font-weight-regular"><br />Register my household</p></span>
+								</div>
 							</v-btn>
 						</template>
 						<span>A household is composed of any people who occupy the same given housing unit. If you are planning to register your household, all members must currently be residing in the same address. If members of a household are residing in other housing units, it is necessary to register any given members individually. Max household registration of 20 registrants.</span>
@@ -77,9 +69,6 @@
 		</v-row>
 	</v-container>
 </template>
-
-
-
 <script>
 import EventBus from '../eventBus'
 
@@ -90,45 +79,41 @@ export default {
 		preferredLanguage: ''
 	
 	}),
-      
-     
- 
-	methods: {
-  
-	singleRegistration()
-		{
-			this.$emit("singleRegistration");
-		},
-		householdRegistration()
-		{
-			this.$emit("householdRegistration");
-		},
-	sendGreetingPageInfoToReviewPage()
+	methods: 
 	{
-		const greetingPagePayload = {
-		preferredLanguage: this.preferredLanguage,
+		singleRegistration()
+			{
+				this.$emit("singleRegistration");
+			},
+			householdRegistration()
+			{
+				this.$emit("householdRegistration");
+			},
+		sendGreetingPageInfoToReviewPage()
+		{
+			const greetingPagePayload = {
+				preferredLanguage: this.preferredLanguage,
+			}
+			EventBus.$emit('DATA_LANGUAGE_INFO_PUBLISHED', greetingPagePayload)
+		}, 
+		verifyFormContents()
+		{
+			var valid = true
+			var message = "Woops! You need to enter the following field:"
+		
+				if (this.preferredLanguage == "") {
+					message += " Preferred Language"
+					valid = false
+				}
+				if (valid == false) {
+					alert(message)
+					return false
+				}
+		
+			this.sendGreetingPageInfoToReviewPage();
+			return true;
 		}
-		EventBus.$emit('DATA_LANGUAGE_INFO_PUBLISHED', greetingPagePayload)
-	}, 
-	verifyFormContents()
-    {
-		//add logic to check form contents
-		var valid = true
-		var message = "Woops! You need to enter the following field:"
-	
-			if (this.preferredLanguage == "") {
-			message += " Preferred Language"
-			valid = false
-			}
-			if (valid == false) {
-				alert(message)
-				return false
-			}
-	
-      this.sendGreetingPageInfoToReviewPage();
-      return true;
-    }
-  },
+	},
 } 
 </script>
 
