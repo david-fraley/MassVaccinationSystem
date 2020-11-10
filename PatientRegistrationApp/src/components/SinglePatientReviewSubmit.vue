15d3d41242527fb922bb6cdfcffb4316a0e1be86
grohde1015 <60712841+grohde1015@users.noreply.github.com>
2020-11-09 21:21:30 -0600

    Revised and removed text
    Revised text and removed secondary heading from the review and submit page

--- PatientRegistrationApp/src/components/SinglePatientReviewSubmit.vue
+++ PatientRegistrationApp/src/components/SinglePatientReviewSubmit.vue
@@ -1,7 +1,6 @@
 <template>
 	<v-container fluid>
 		<v-row align="center" justify="center">
-			<v-heading> Ensure information is correct before submission. </v-heading>
 		</v-row>
 		<v-row>
 			<v-col cols="12" sm="6"	md="3">
