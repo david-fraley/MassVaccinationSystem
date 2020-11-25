c955776009b2dc51f0337dcda4015eaea30423ab
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2020-11-24 18:33:27 -0600

    Update formatting

--- PointOfDispensingApp/src/components/VaccinationScreeningComponent.vue
+++ PointOfDispensingApp/src/components/VaccinationScreeningComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-container>
-    <v-row>
+  <v-container fill-height fluid>
+    <v-row no-gutters>
       <v-col cols="6">
-        <v-row>
+        <v-row no-gutters>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group>
                 <template v-slot:label>
                   <div>Patient info confirmed? </div>
@@ -13,7 +13,7 @@
             </v-row>
           </v-col>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group v-model="patientInfoComfirmed" row>
                   <v-radio
                     label="Yes"
@@ -27,9 +27,9 @@
             </v-row>
           </v-col>
         </v-row> 
-        <v-row>
+        <v-row no-gutters>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group>
                 <template v-slot:label>
                   <div>Consent form signed?</div>
@@ -38,7 +38,7 @@
             </v-row>
           </v-col>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group v-model="consentFormSigned" row>
                   <v-radio
                     label="Yes"
@@ -52,9 +52,9 @@
             </v-row>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row no-gutters>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group>
                 <template v-slot:label>
                   <div>Proper screening completed?</div>
@@ -63,7 +63,7 @@
             </v-row>
           </v-col>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group v-model="screeningCompleted" row>
                   <v-radio
                     label="Yes"
@@ -77,9 +77,9 @@
             </v-row>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row no-gutters>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group>
                 <template v-slot:label>
                   <div>VIS fact sheet provided?</div>
@@ -88,7 +88,7 @@
             </v-row>
           </v-col>
           <v-col cols="6">
-            <v-row>
+            <v-row no-gutters>
               <v-radio-group v-model="factSheetProvided" row>
                   <v-radio
                     label="Yes"
@@ -105,22 +105,24 @@
       </v-col>
                 
       <v-col cols="6">
-        <v-card color="accent" dark>
-          <v-card-title style="center" class="headline">
+        <v-card color="accent" dark height="50%" v-show="true">
+          <v-card-title class="headline justify-center">
               Proceed with vaccination?
           </v-card-title>
           <v-row align="center" justify="center">
-            <v-col cols="12">
-              <v-radio-group v-model="row" row>
+            <v-col cols="2">
                 <v-radio
+                  v-model="vaccinationYes"
                   label="Yes"
                   value="Yes"
                 ></v-radio>
+            </v-col>
+            <v-col cols="2">   
                 <v-radio
+                  v-model="vaccinationNo"
                   label="No"
                   value="No"
                 ></v-radio>
-              </v-radio-group>
             </v-col>
           </v-row>
         </v-card>
