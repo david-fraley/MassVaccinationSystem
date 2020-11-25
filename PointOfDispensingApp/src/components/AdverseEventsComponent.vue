372a15ca058789118572893156975e7b63453cde
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2020-11-25 17:39:41 -0600

    [MVS-427] Populate/format AdverseEventsComponent.vue page

--- PointOfDispensingApp/src/components/AdverseEventsComponent.vue
+++ PointOfDispensingApp/src/components/AdverseEventsComponent.vue
@@ -1,11 +1,25 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="4">
-        Vaccination status (e.g. not done) goes here
-      </v-col>
-      <v-col cols="4">
-        Vaccination time stamp goes here
+      <v-col cols="6">
+		<v-text-field
+			placeholder="Complete"
+			label="Vaccination Status" 
+			id="vaccinationStatus" 
+			v-model="vaccinationStatus"
+			readonly
+			filled
+			prepend-icon="mdi-menu-right"
+		></v-text-field></v-col>
+      <v-col cols="6">
+		<v-text-field
+			placeholder="MM/DD/YYYY   HH/MM/SS"
+			label="Completion Time" 
+			id="completionTime" 
+			v-model="completionTime"
+			readonly
+			filled
+		></v-text-field>
       </v-col>
     </v-row>
     <v-row>
@@ -13,11 +27,186 @@
         <v-divider></v-divider>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12">
-        Adverse reactions inputs go here
-      </v-col>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
     </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+		<v-row>
+		<v-col cols="4">
+		<v-select
+			v-model="reactionList"
+			:items="reactionList"
+			label="Reaction List"
+			prepend-icon="mdi-menu-right"
+        ></v-select></v-col>
+
+		<v-col cols="4">
+        <v-text-field 
+			id="adverseReactionTimeStamp" 
+			v-model="adverseReactionTimeStamp"
+			placeholder="MM/DD/YYYY HH/MM/SS"
+        ></v-text-field></v-col>
+		
+		<v-col cols="4">
+        <v-text-field 
+			id="notes" 
+			v-model="notes"
+			placeholder="If other, list reaction"
+        ></v-text-field></v-col>
+    </v-row>
+	
+	<v-row>
+	<v-col cols="12">
+		<v-textarea
+		id="moreReactions"
+		placeholder="If more reactions, please list here"
+		outlined
+		rows="3"
+		></v-textarea>
+		</v-col>
+    </v-row>
+	
     <v-row align="center" justify="center">
       <v-col cols="6">
         <v-btn block color="accent">
@@ -38,7 +227,13 @@
     {
     },
     data () {
-      return {
+		return {
+		reactionList: [
+			"Rash",
+			"Trouble breathing",
+			"Fever",
+			"Fainting",
+			],
       }
     }
   }
