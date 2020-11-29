9c3610a775fa40da1efaf2045f76786c17d88152
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2020-11-29 10:46:00 -0600

    [MVS-416] Use another method on single and household home address page

--- PatientRegistrationApp/src/components/SinglePatientHomeAddress.vue
+++ PatientRegistrationApp/src/components/SinglePatientHomeAddress.vue
@@ -17,13 +17,14 @@
 				<v-text-field
 					id = "addr"
 					required
-					class="required"
 					:rules="[v => !!v || 'Address field is required']"
-					label="Home Address"
 					v-model="lineAddress"
 					v-show="homeAddressAvailable"
-					prepend-icon="mdi-menu-right"
-				></v-text-field>
+					prepend-icon="mdi-menu-right">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>Home Address
+						</template>
+				</v-text-field>
 			</v-col>
 		</v-row>
 		<v-row>
@@ -31,59 +32,64 @@
 				<v-text-field
 					id = "city"
 					required
-					class="required"
 					:rules="[v => !!v || 'City field is required']"
-					label="City"
 					v-model="cityAddress"
 					v-show="homeAddressAvailable"
-					prepend-icon="mdi-menu-right"
-				></v-text-field>
+					prepend-icon="mdi-menu-right">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>City
+						</template>
+				</v-text-field>
 			</v-col>
 				<v-col class="d-flex" cols="1" sm="1" md="1">
 				<v-select
 				id = "state"
 					required
-					class="required"
 					:rules="[v => !!v || 'State field is required']"
 					v-model="stateAddress"
 					v-show="homeAddressAvailable"
-                    :items="state"
-                    label="State"
-				></v-select>
+                    :items="state">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>State
+						</template>
+				</v-select>
 			</v-col>
 			<v-col class="d-flex" cols="3" sm="3">
 				<v-text-field
 				id = "county"
 					required
-					class="required"
 					:rules="[v => !!v || 'County field is required']"
-                    label="County"
 					v-model="districtAddress"
-					v-show="homeAddressAvailable"
-				></v-text-field>
+					v-show="homeAddressAvailable">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>County
+						</template>
+				</v-text-field>
 			</v-col>
 			<v-col class="d-flex" cols="2" sm="2">
 				<v-select
 				id = "country"
 					required
-					class="required"
 					:rules="[v => !!v || 'Country field is required']"
 					v-model="countryAddress"
 					v-show="homeAddressAvailable"
-                    :items="country"
-                    label="Country"
-				></v-select>
+                    :items="country">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>Country
+						</template>
+				</v-select>
 			</v-col>
 			<v-col cols="3" sm="3" md="3">
 				<v-text-field
 					id = "zipcode"
 					required
-					class="required"
 					:rules="postalCodeRules"
-					label="Zipcode"
 					v-model="postalCode"
-					v-show="homeAddressAvailable"					
-				></v-text-field>
+					v-show="homeAddressAvailable">
+						<template #label>
+						<span class="red--text"><strong>* </strong></span>Zipcode
+						</template>
+				</v-text-field>
 			</v-col>
 		</v-row>	
 	</v-container>
@@ -265,11 +271,3 @@ import EventBus from '../eventBus'
 	},
 }
 </script>
-
-<style lang="css" scoped>
-.required:before{
-	content:"*";
-	color: red;
-	font-weight:bold;
-}
-</style>
