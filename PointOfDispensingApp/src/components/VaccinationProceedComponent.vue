b20b1e861d3218c14a7f80b4f5697cea20e5eb85
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2020-11-30 08:55:09 -0600

    Update formatting of VaccinationProceedComponent

--- PointOfDispensingApp/src/components/VaccinationProceedComponent.vue
+++ PointOfDispensingApp/src/components/VaccinationProceedComponent.vue
@@ -8,64 +8,88 @@
       </v-col>
     </v-row>
     <v-row>
-		<v-col cols="6">
-		<v-text-field 
-			label="LOT Number" 
-			id="LOTNumber" 
-			required
-			class="required"
-			:rules="[v => !!v || 'LOT Number field is required']"
-			v-model="LOTNumber"
-			prepend-icon="mdi-menu-right"
-        ></v-text-field> </v-col>
-		<v-col cols="5">
-		<v-select
-			v-model="DoseQty"
-			class="required"
-			:items="DoseQty"
-			label="Dose qty."
-        ></v-select></v-col>
-	</v-row>
-		
-		
-	<v-row>
-	<v-col cols="6">
-		<v-text-field 
-			label="Expiration Date" 
-			id="ExpDate" 
-			required
-			:rules="[v => !!v || 'Expiration Date field is required']"
-			v-model="ExpDate"
-			prepend-icon="mdi-menu-right"
-        ></v-text-field></v-col>
-	<v-col cols="6">
-		<v-text-field
-			v-model="DoseNumber"
-			required
-			class="required"
-			:rules="[v => !!v || 'Dose number field is required']"
-			id="DoseNumber"
-			label="Dose number"
-        ></v-text-field></v-col>
-	</v-row>
-	
-	<v-row>
-	<v-col cols="6">
-        <v-text-field 
-			label="Manufacturer" 
-			id="Manufacturer" 
-			required
-			class="required"
-			:rules="[v => !!v || 'Manufacturer field is required']"
-			v-model="Manufacturer"
-			prepend-icon="mdi-menu-right"
-        ></v-text-field></v-col>
-	</v-row>
-
+      <v-col cols="5">
+        <v-row no-gutters>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Lot Number</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense
+              value="####"
+              id="LotNumber" 
+              required
+              :rules="[v => !!v || 'Lot Number field is required']"
+              v-model="LotNumber"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Expiration Date</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense
+              value="MM/DD/YYYY"
+              id="ExpDate" 
+              required
+              :rules="[v => !!v || 'Expiration Date field is required']"
+              v-model="ExpDate"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Manufacturer</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense
+              value="Pfizer"
+              id="Manufacturer" 
+              required
+              :rules="[v => !!v || 'Manufacturer field is required']"
+              v-model="Manufacturer"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+      <!--blank column for spacing-->
+      <v-col cols="1">
+      </v-col>
+      <v-col cols="5">
+        <v-row no-gutters>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Dose Quantity</div>
+          </v-col>
+          <v-col cols="8">
+            <v-select
+              :items="doseQuantityOptions"
+              outlined
+              dense
+              required
+              :rules="[v => !!v || 'Dose Quantity field is required']"
+              v-model="DoseQty"
+            ></v-select>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Dose Number</div>
+          </v-col>
+          <v-col cols="8">
+            <v-select
+              :items="doseNumberOptions"
+              outlined
+              dense
+              v-model="DoseNumber"
+              required
+              :rules="[v => !!v || 'Dose Number field is required']"
+              id="DoseNumber"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>    
     <v-row>
       <v-col cols="12">
         <v-divider></v-divider>
       </v-col>
+    </v-row>
+    <v-row>
+      <p> </p> <!--blank row for spacing-->
     </v-row>
     <v-row>
       <v-col cols="12">
@@ -76,71 +100,79 @@
     </v-row>
 	
     <v-row>
-		<v-col cols="6">
-        <v-text-field 
-			label="Vaccination Status" 
-			id="vaccinationStatus" 
-			required
-			:rules="[v => !!v || 'Vaccination Status field is required']"
-			v-model="vaccinationStatus"
-			prepend-icon="mdi-menu-right"
-        ></v-text-field></v-col>
-		<v-col cols="6">
-		<v-text-field 
-			label="Vaccination Time Stamp" 
-			id="vaccinationTimeStamp" 
-			required
-			:rules="[v => !!v || 'Vaccination Time Stamp field is required']"
-			v-model="vaccinationTimeStamp"
-        ></v-text-field></v-col>
+      <v-col cols="5">
+        <v-row no-gutters>
+          <v-col cols="4">
+            <div class="secondary--text">Vaccination Status</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense filled readonly
+              value="completed"
+              id="vaccinationStatus"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text">Healthcare Practitioner</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense filled readonly
+              value="Doogie Howser"
+              id="healthPractitioner" 
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text"><span style="color:red">*</span>Site of Vaccination</div>
+          </v-col>
+          <v-col cols="8">
+            <v-select
+              :items="vaccinationSiteOptions"
+              outlined
+              dense
+              required
+              :rules="[v => !!v || 'Site of Vaccination field is required']"
+              v-model="siteOfVaccination"
+            ></v-select>
+          </v-col>
+          <v-col cols="4">
+            <div class="secondary--text">Route</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense filled readonly
+              value="Injection"
+            ></v-text-field>
+          </v-col>
         </v-row>
-		
-	<v-row>
-		<v-col cols="6">
-        <v-text-field 
-			label="Health practitioner" 
-			id="healthPractitioner" 
-			required
-			:rules="[v => !!v || 'Health practitioner field is required']"
-			v-model="healthPractitioner"
-			prepend-icon="mdi-menu-right"
-        ></v-text-field></v-col>
-	</v-row>
-	
-	<v-row>
-        <v-col cols="6">
-		<v-select
-			v-model="siteOfVaccination"
-			class="required"
-			:items="siteOfVaccination"
-			label="Site of vaccination"
-			prepend-icon="mdi-menu-right"
-        ></v-select></v-col>
-	</v-row>
-	
-	<v-row>
-        <v-col cols="6">
-		<v-select
-			v-model="route"
-			:items="route"
-			label="Route"
-			prepend-icon="mdi-menu-right"
-        ></v-select></v-col>
-	</v-row>
-
+      </v-col>
+      <!--blank column for spacing-->
+      <v-col cols="1">
+      </v-col>
+      <v-col cols="5">
+        <v-row no-gutters>
+          <v-col cols="4">
+            <div class="secondary--text">Date/Time</div>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field outlined dense filled readonly
+              value="04/21/2021  11:13:27"
+              id="vaccinationStatus" 
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
     <v-row>
-		<v-col cols="12">
-		<v-textarea
-		id="notes"
-		placeholder="Notes"
-		outlined
-		rows="4"
-		></v-textarea>
-		</v-col>
+      <v-col cols="12">
+        <v-textarea
+          id="notes"
+          placeholder="Notes"
+          outlined
+          rows="4"
+        ></v-textarea>
+      </v-col>
     </v-row>
     <v-row align="center" justify="center">
       <v-col cols="6">
-        <v-btn block color="accent">
+        <v-btn block color="accent" @click="submitVaccinationRecord()">
           Submit vaccination record
         </v-btn>
       </v-col>
@@ -153,43 +185,19 @@
     name: 'VaccinationProceedComponent',
     methods: 
     {
+      submitVaccinationRecord() {
+        alert('You clicked Submit Vaccination Record')
+      }
     },
     components: 
     {
     },
     data () {
-		return {
-			DoseQty: [
-			"0.1 mL",
-			"0.2 mL",
-			"0.5 mL",
-			"1 mL",
-			],
-			
-			siteOfVaccination: [
-			"Left arm",
-			"Right arm",
-			],
-			
-			route: [
-			"IDINJ (injection, intradermal)",
-			"IM (injection, intramuscular)",
-			"NASINHLC (inhalation, nasal) INVINJ (injection, intravenous)",
-			"PO (swallow, oral)",
-			"SQ (injection, subcutaneous)",
-			"TRNSDERM (transdermal)",
-			],
-			
-		}
+      return {
+        doseNumberOptions: ['1', '2'],
+        doseQuantityOptions: ['0.1 mL', '0.2 mL', '0.5 mL', '1.0 mL'],
+        vaccinationSiteOptions: ['Left arm', 'Right arm'],
+      }
     }
   }
 </script>
-
-<style lang="css" scoped>
-.required:before{
-	content:"*";
-	color: red;
-	font-weight:bold;
-	float: left;
-}
-</style>
