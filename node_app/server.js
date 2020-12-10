f208c381904bc9cf72454b1889a610b566055fb7
adelk719 <kalalbeka719@gmail.com>
2020-12-10 15:24:16 -0800

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -69,39 +69,35 @@ app.post("/Practitioner", (req, res) => {
 
 
 
-// Pass request to HAPI FHIR server
-app.all('/Patient*', (req, res) => {
-
-    axios({
-        method: req.method,
-        url: `${base}${req.url}`,
-        data: req.body,
-        headers: headers
-    })
+app.get("/Practitioner*", (req, res) => {
+  axios({
+    method: req.method,
+    url: `${base}${req.url}`,
+    data: req.body,
+    headers: headers,
+  })
     .then((response) => {
-        // handle success
-        res.json(response.data);
+      // handle success
+      res.json(response.data);
     })
     .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            let errorCode = error.response.status;
-            res.status(errorCode).send(error.response.data);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        let errorCode = error.response.status;
+        res.status(errorCode).send(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
     })
     .then(function () {
-        // always executed
+      // always executed
     });
 });
-
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`));
