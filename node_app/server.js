7fe8f7c4c5edb5a077ab77f2af93b208d910b8a0
adelk719 <kalalbeka719@gmail.com>
2020-12-10 14:45:32 -0800

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -11,7 +11,7 @@ let headers = {
 }
 
 
-app.post("/Patient", (req, res) => {
+app.post("/Practitioner", (req, res) => {
   let obj = req.body;
   for (practitioner of obj.Practitioner) {
     let resource = {
