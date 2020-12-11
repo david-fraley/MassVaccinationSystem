e6c99c525f363f6ca3b39fd6cdb902d7625b39b7
adelk719 <kalalbeka719@gmail.com>
2020-12-11 12:12:19 -0800

    Update server.js

--- broker/server.js
+++ broker/server.js
@@ -2,6 +2,7 @@
 const configs = require("./config/server.js");
 
 const express = require("express");
+const app = express();
 const axios = require("axios").default;
 const Patient = require("./endpoints/Patient");
 const Organization = require("./models/Organization");
@@ -13,7 +14,6 @@ const EpisodeOfCare = require("./models/EpisodeOfCare");
 const Practitioner = require("./models/Practitioner");
 const SendHL7Message = require("./endpoints/SendHL7Message");
 
-const app = express();
 app.use(express.json());
 app.set("json spaces", 2);
 
