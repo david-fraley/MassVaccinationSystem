ed118e67de654ce92c3cd0b51c2186ebfe2f6b38
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2020-12-10 18:30:48 -0600

    [MVS-495] Add a vue page for HouseholdFollowUp

---
+++ PatientRegistrationApp/src/components/HouseholdFollowUp.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-container fluid>
+		<v-row align="center" justify="center">
+		</v-row>
+		<v-row>
+			<v-col cols="12" sm="6"	md="3">
+				<div class="font-weight-medium primary--text"> QR Code Page</div>
+			</v-col>
+		</v-row>
+
+	</v-container>
+</template>
+ 
+<script>
+import EventBus from '../eventBus'
+
+	export default {
+	data () {
+		return {
+			
+		}
+	},
+	methods:
+	{
+		
+	},
+}
+</script>
