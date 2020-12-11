7c94974e039f4846302b1c2e8b44f5cb6a7ed8a8
adelk719 <kalalbeka719@gmail.com>
2020-12-10 16:50:08 -0800

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -1,75 +1,18 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios").default;
+
 const app = express();
-const axios = require('axios').default;
-const base = 'http://hapi:8080/hapi-fhir-jpaserver/fhir'
-
 app.use(express.json());
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
-let headers = {
-    'content-type': 'application/fhir+json'
-}
+const base = "http://hapi:8080/hapi-fhir-jpaserver/fhir";
+const generalEndpoints = ["/Patient*", "/Organization*"];
+const headers = {
+  "content-type": "application/fhir+json",
+};
 
-
-app.post("/Practitioner", (req, res) => {
-    let prac= req.body.Practitioner;
-        let resource = {
-            resourceType: "Practitioner",
-            name: [
-             {
-                family: prac.family,
-                given: [prac.given],
-                suffix: [prac.suffix]
-             }
-            ],
-            telecom: [ //add later
-                ],
-            gender: prac.gender,
-            birthdate: prac.birthdate,
-            address: [
-                {
-                    line: [prac.address.line],
-                    city: prac.address.city,
-                    state: prac.address.state,
-                    postalCode: prac.address.postalCode,
-                    country: prac.address.country,
-                },
-            ],
-            photo: [
-                {
-                    url: "", //add later
-                    title: 'Photo of ${prac.given} ${prac.family} ${prac.suffix}',
-                },
-            ],
-            qualification: [
-                {
-                    code: {
-                            coding: [
-                                {
-                                    code: prac.qualificationCode
-                                }
-                            ],
-                            text: "",
-
-                    }
-                    
-                }
-            ],            
-
-        };
-        
-    axios
-    .post(`${base}/Practitioner`, resource, headers)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((e) => res.send(e));
-
-});
-
-
-
-app.get("/Practitioner*", (req, res) => {
+// Pass GET requests to HAPI FHIR server
+app.get(generalEndpoints, (req, res) => {
   axios({
     method: req.method,
     url: `${base}${req.url}`,
@@ -80,24 +23,8 @@ app.get("/Practitioner*", (req, res) => {
       // handle success
       res.json(response.data);
     })
-    .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        let errorCode = error.response.status;
-        res.status(errorCode).send(error.response.data);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    })
-    .then(function () {
-      // always executed
-    });
+    .catch((error) => handleError(res, error));
 });
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
