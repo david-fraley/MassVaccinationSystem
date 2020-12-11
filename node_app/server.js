835625e7450c26a28afdedc0c3560d485467db3e
adelk719 <kalalbeka719@gmail.com>
2020-12-11 12:12:19 -0800

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -1,31 +1,14 @@
 const express = require("express");
-const axios = require("axios").default;
-
 const app = express();
+const axios = require("axios").default;
+const base = "http://hapi:8080/hapi-fhir-jpaserver/fhir";
+
 app.use(express.json());
 app.set("json spaces", 2);
 
-const base = "http://hapi:8080/hapi-fhir-jpaserver/fhir";
-const generalEndpoints = ["/Patient*", "/Organization*", "/Practitioner*", "/Location*", "/EpisodeOfCare*"];
-const headers = {
+let headers = {
   "content-type": "application/fhir+json",
 };
-
-// Pass GET requests to HAPI FHIR server
-app.get(generalEndpoints, (req, res) => {
-  axios({
-    method: req.method,
-    url: `${base}${req.url}`,
-    data: req.body,
-    headers: headers,
-  })
-    .then((response) => {
-      // handle success
-      res.json(response.data);
-    })
-    .catch((error) => handleError(res, error));
-});
-
 
 app.post("/Location", (req, res) => {
     let loc = req.body.Location;
