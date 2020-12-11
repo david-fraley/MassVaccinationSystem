083eaff2b8e19d061de4a3940d11c989d8ddcac3
adelk719 <kalalbeka719@gmail.com>
2020-12-10 17:28:12 -0800

    Update server.js

--- node_app/server.js
+++ node_app/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.set("json spaces", 2);
 
 const base = "http://hapi:8080/hapi-fhir-jpaserver/fhir";
-const generalEndpoints = ["/Patient*", "/Organization*"];
+const generalEndpoints = ["/Patient*", "/Organization*", "/Practitioner*", "/Location*"];
 const headers = {
   "content-type": "application/fhir+json",
 };
@@ -27,4 +27,158 @@ app.get(generalEndpoints, (req, res) => {
 });
 
 
+app.post("/Practitioner", (req, res) => {
+    let prt = req.body.Practitioner;
+    let resource = {
+        resourceType: "Practitioner",
+        name: [
+            {
+                family: prt.family,
+                given: [prt.given],
+                suffix: [prt.suffix],
+            },
+        ],
 
+        gender: prt.gender,
+        birthDate: prt.birthdate,
+
+        qualification: [
+        {
+            code: {
+                coding: [
+                            {
+                                code: prt.qualificationCode,
+                                display: prt.QualificationCode,
+                            },
+                        ],
+                    },
+
+                },
+            ],
+    };
+
+    axios
+    .post(`${base}/Practitioner`, resource, headers)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((e) => res.send(e));
+
+
+});
+
+// Check-in given either
+// a) appointment id from QR code
+// b) patient id from patient lookup
+app.get("/check-in", (req, res) => {
+  const encounter_status = "arrived";
+  const appointment_status = "arrived";
+
+  if (req.query.hasOwnProperty("patient")) {
+    updateEncounterStatus(
+      `?subject=${req.query.patient}&status=planned`,
+      encounter_status,
+      res
+    );
+    updateAppointmentStatus(
+      `?actor=${req.query.patient}&status=booked`,
+      appointment_status,
+      res
+    );
+    res.send("");
+  } else if (req.query.hasOwnProperty("appointment")) {
+    updateEncounterStatus(
+      `?appointment=${req.query.appointment}`,
+      encounter_status,
+      res
+    );
+    updateAppointmentStatus(
+      `/${req.query.appointment}`,
+      appointment_status,
+      res
+    );
+    res.send("");
+  } else {
+    res.send("error");
+  }
+});
+
+// Given url of Encounter and new status, update encounter resource
+// URL is either a specific id '/id'
+// or query parameters '?param=value'
+function updateEncounterStatus(url, status, res) {
+  axios
+    .get(`${base}/Encounter${url}`)
+    .then((response) => {
+      try {
+        let encounter;
+        if (url.startsWith("/")) {
+          encounter = response.data;
+        } else {
+          let bundle = response.data;
+          // update status
+          encounter = bundle.entry[0].resource;
+        }
+        encounter.status = status;
+        // update status history
+
+        // update the database with new encounter
+        axios
+          .put(`${base}/Encounter/${encounter.id}`, encounter, headers)
+          .catch((error) => handleError(res, error));
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .catch((error) => handleError(res, error))
+    .then(function () {
+      // always executed
+    });
+}
+
+function updateAppointmentStatus(url, status, res) {
+  axios
+    .get(`${base}/Appointment${url}`)
+    .then((response) => {
+      try {
+        let appt;
+        if (url.startsWith("/")) {
+          appt = response.data;
+        } else {
+          let bundle = response.data;
+          // update status
+          appt = bundle.entry[0].resource;
+        }
+        appt.status = status;
+
+        // update the database with new appointment
+        axios
+          .put(`${base}/Appointment/${appt.id}`, appt, headers)
+          .catch((error) => handleError(res, error));
+      } catch (e) {
+        console.log(e);
+      }
+    })
+    .catch((error) => handleError(res, error))
+    .then(function () {
+      // always executed
+    });
+}
+
+function handleError(res, error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    let errorCode = error.response.status;
+    res.status(errorCode).send(error.response.data);
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log("Error", error.message);
+  }
+  console.log(error.config);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
