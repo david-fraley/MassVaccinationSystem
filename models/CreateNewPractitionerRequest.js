a8099439eb22f2359bddf542769d2f609b4abc8d
adelk719 <kalalbeka719@gmail.com>
2020-12-12 11:53:04 -0800

    Create CreateNewPractitionerRequest.js

---
+++ models/CreateNewPractitionerRequest.js
@@ -0,0 +1,40 @@
+/*
+Practitioner {
+  family : string
+  given : string
+  suffix : string
+  phone : string
+  email : string
+  gender : enum (male, female, other, unknown)
+  birthdate : date
+  address : {
+    line: string
+    city: string
+    state: string
+    postalCode: string
+    country: string
+  }
+  qualificationCode : enum (BN, CANP, CER, CMA, CNP, CNS, CRN, EMT, RN, RMA, PN, RPH)
+  }
+}
+{
+  "Practitioner":
+    {
+      "family" : "George",
+      "given" : "James",
+      "suffix" : "Sr.",
+      "phone" : "(010)000-000"
+      "email" : "email@site.com"
+      "gender": "male",
+      "birthDate": "2000-01-01",
+      "address": {
+        "line": "97 Walnut St",
+        "city": "New York City",
+        "state": "NY",
+        "postalCode": "14623",
+        "country": "USA"
+      },
+      "qualificationCode": "CER"
+    }
+}
+*/
