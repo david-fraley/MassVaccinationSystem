8b0557d16ff3499f1933145cfe25c023e4d56f90
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-12-21 14:06:01 -0500

    Create module for model to FHIR format

--- node_app/server.js
+++ node_app/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios").default;
+const Organization = require("./models/Organization");
 
 const app = express();
 app.use(express.json());
@@ -13,12 +14,8 @@ const headers = {
 
 // Pass GET requests to HAPI FHIR server
 app.get(generalEndpoints, (req, res) => {
-  axios({
-    method: req.method,
-    url: `${base}${req.url}`,
-    data: req.body,
-    headers: headers,
-  })
+  axios
+    .get(`${base}${req.url}`)
     .then((response) => {
       // handle success
       res.json(response.data);
@@ -28,22 +25,7 @@ app.get(generalEndpoints, (req, res) => {
 
 app.post("/Organization", (req, res) => {
   let org = req.body.Organization;
-  let resource = {
-    resourceType: "Organization",
-    active: org.active,
-    type: [
-      {
-        coding: [
-          {
-            system: "http://hl7.org/fhir/ValueSet/organization-type",
-            code: org.type,
-            display: org.type,
-          },
-        ],
-      },
-    ],
-    name: org.name,
-  };
+  let resource = Organization.toFHIR(org);
 
   // post resource
   axios
@@ -52,100 +34,6 @@ app.post("/Organization", (req, res) => {
       res.json(response.data);
     })
     .catch((e) => res.send(e));
-
-app.post("/Patient", (req, res) => {
-  let obj = req.body;
-  for (patient of obj.Patient) {
-    let resource = {
-      resourceType: "Patient",
-      name: [
-        {
-          family: patient.family,
-          given: [patient.given],
-          suffix: [patient.suffix],
-        },
-      ],
-      telecom: [
-        // add later
-      ],
-      gender: patient.gender,
-      birthDate: patient.birthDate,
-      address: [
-        {
-          line: [patient.address.line],
-          city: patient.address.city,
-          state: patient.address.state,
-          postalCode: patient.address.postalCode,
-          country: patient.address.country,
-        },
-      ],
-      photo: [
-        {
-          url: "", // add later
-          title: `Photo of ${patient.given} ${patient.family} ${patient.suffix}`,
-        },
-      ],
-      contact: [
-        {
-          relationship: [
-            {
-              coding: [
-                {
-                  // unsure
-                  system: "http://terminology.hl7.org/CodeSystem/v2-0131",
-                  code: patient.contact.relationship,
-                  display: patient.contact.relationship,
-                },
-              ],
-            },
-          ],
-          name: {
-            family: patient.contact.family,
-            given: [patient.contact.given],
-          },
-          telecom: [
-            {
-              system: "phone",
-              value: patient.contact.phone.value,
-              use: patient.contact.phone.use,
-            },
-          ],
-        },
-      ],
-      communication: [
-        {
-          language: {
-            text: patient.language,
-          },
-          preferred: true,
-        },
-      ],
-    };
-    // add in telecom
-    for (idx in patient.phone) {
-      resource.telecom.push({
-        system: "phone",
-        value: patient.phone[idx].value,
-        use: patient.phone[idx].use,
-        rank: `${idx}`,
-      });
-    }
-    for (idx in patient.email) {
-      resource.telecom.push({
-        system: "email",
-        value: patient.email[idx],
-        rank: `${idx}`,
-      });
-    }
-
-    // post resource
-    axios
-      .post(`${base}/Patient`, resource, headers)
-      .then((response) => {
-        res.json(response.data);
-      })
-      .catch((e) => res.send(e));
-  }
 });
 
 // Check-in given either
