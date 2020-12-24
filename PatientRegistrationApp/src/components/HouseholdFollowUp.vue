6e21910f6a0050f51d747517da95f074f89f504e
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2020-12-24 09:39:57 -0600

    [MVS-499] format Household member 1 in QR code page

--- PatientRegistrationApp/src/components/HouseholdFollowUp.vue
+++ PatientRegistrationApp/src/components/HouseholdFollowUp.vue
@@ -2,26 +2,73 @@
 	<v-container fluid>
 		<v-row align="center" justify="center">
 		</v-row>
-		<v-row>
-			<v-col cols="12" sm="6"	md="3">
-				<div class="font-weight-medium primary--text"> QR Code Page</div>
+		<v-row>	
+			<v-col cols="12">
+				<v-card class="d-flex flex-wrap" flat>
+					<v-card class="pa-2" flat min-width=33%>
+						<div class="font-weight-medium primary--text">Household Member #1</div>
+						<div> QR Code Placeholder</div>
+						<div class="font-weight-medium">Name:  <span class="font-weight-regular">{{dataHouseholdPersonalInfo[0].familyName}}, 
+						{{dataHouseholdPersonalInfo[0].givenName}} {{dataHouseholdPersonalInfo[0].middleName}} {{dataHouseholdPersonalInfo[0].suffix}}</span></div>
+						<v-card-actions>
+							<v-btn outlined small color="primary">
+								Download
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+					
+				</v-card>
 			</v-col>
 		</v-row>
-
+		<v-row>
+			<v-col cols="12">
+			<p> </p>
+			</v-col>
+		</v-row>
 	</v-container>
 </template>
- 
-<script>
 
-export default {
-data () {
-	return {
-			
-	}
+<script>
+import EventBus from '../eventBus'
+
+	export default {
+	data () {
+		return {
+			dataHouseholdPersonalInfo: []
+		}
 	},
-methods:
+	props:
 	{
-		
+		numberOfHouseholdMembers: Number
+	},
+	methods:
+	{
+		updateHouseholdPersonalInfoData(householdPersonalInfoPayload, householdMemberNumber) {
+			// if member is next in line, add to array of members
+			if (householdMemberNumber-1 == this.dataHouseholdPersonalInfo.length) {
+				this.dataHouseholdPersonalInfo.push(householdPersonalInfoPayload);
+			// else if member already exists in array, update the member
+			} else if (householdMemberNumber-1 < this.dataHouseholdPersonalInfo.length) {
+				this.$set(this.dataHouseholdPersonalInfo, householdMemberNumber-1, householdPersonalInfoPayload);
+			}
+		},
+		getNumberOfHouseholdMembers()
+		{
+			return this.numberOfHouseholdMembers;
+		},
+	},
+	mounted() 
+	{
+		EventBus.$on('DATA_HOUSEHOLD_PERSONAL_INFO_PUBLISHED', (householdPersonalInfoPayload, householdMemberNumber) => {
+			this.updateHouseholdPersonalInfoData(householdPersonalInfoPayload, householdMemberNumber)
+		})
 	},
 }
 </script>
+<style lang="css" scoped>
+
+	.hidden {
+		display: none;
+	}
+
+</style>
