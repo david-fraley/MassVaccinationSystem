e35d460243ae082790f02cad2aac35653d5d1c6c
Todd Jensen <todd@jenseninformatics.com>
2020-12-30 15:21:10 -0600

    MVS-490 - setting up configs for reading secrets from local .env file.
    This will be needed to connect to a database or other source for patient ID generation.

---
+++ broker/config/server.js
@@ -0,0 +1,12 @@
+// Read private configuration settings from .env file into process.env
+//
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Export configuration settings with some values set to defaults.
+// NOTE: login/password settings should not be defaulted.
+//
+module.exports = {
+    port: process.env.PORT || 3000,
+    base: process.env.FHIR_URL_BASE || "http://hapi:8080/hapi-fhir-jpaserver/fhir"
+}
