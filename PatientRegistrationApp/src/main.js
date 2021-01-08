d8d4958351abf2622f7e05bf2794bfc33b47271c
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2021-01-08 12:52:49 -0600

    [MVS-539] Remove a not used dependency

--- PatientRegistrationApp/src/main.js
+++ PatientRegistrationApp/src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueMask from 'v-mask';
-import htmlToPdf from 'html2canvas';
 
 Vue.use(VueMask);
-Vue.use(htmlToPdf);
 Vue.config.productionTip = false
 
 new Vue({
