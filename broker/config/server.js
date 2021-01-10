11479af4da695359679dec70b8cc8fd9b8614275
rrucker3 <rob.rucker3@gmail.com>
2021-01-10 14:55:46 -0600

    [MVS-533] Add endpoint for broker to Mirth message

--- broker/config/server.js
+++ broker/config/server.js
@@ -8,5 +8,6 @@ dotenv.config();
 //
 module.exports = {
     port: process.env.PORT || 3000,
-    fhirUrlBase: process.env.FHIR_URL_BASE || "http://hapi:8080/hapi-fhir-jpaserver/fhir"
+    fhirUrlBase: process.env.FHIR_URL_BASE || "http://hapi:8080/hapi-fhir-jpaserver/fhir",
+    mirthUrl: process.env.MIRTH_URL || "http://localhost:8444/"
 }
--- broker/server.js
+++ broker/server.js
@@ -130,6 +130,38 @@ app.post("/Observation", (req, res) => {
     });
 });
 
+// Tell Mirth to send an HL7 message
+app.post("/SendHL7Message", (req, res) => {
+    let immId = req.query.immunization;
+    if (process.env.DEVELOPMENT == 1) {
+        console.log("Immunization ID: " + immId);
+    }
+
+    return axios.get(`${configs.fhirUrlBase}/Immunization?_id=${immId}&_include=Immunization:patient`).then((response) => {
+        let mirthBundle = response.data;
+
+        if (process.env.DEVELOPMENT == 1) {
+            console.log(response);
+        }
+
+        axios.post(`${configs.mirthUrl}`, mirthBundle).then((immResponse) => {
+            if (process.env.DEVELOPMENT == 1) {
+                console.log(immResponse);
+            }
+
+            res.json(immResponse.data);
+        }).catch((e) => {
+            res.status(400).json({
+                error: e.response ? e.response.data : e.message,
+            });
+        });
+    }).catch((e) => {
+        res.status(400).json({
+            error: e.response ? e.response.data : e.message,
+        });
+    });
+});
+
 async function postObservation(observation) {
   let resource = Observation.toFHIR(observation);
 
