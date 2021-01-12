7ed114a9def726a11ec6d939d76a4bb3a4094b5c
ShahdSawalhi <70071991+ShahdSawalhi@users.noreply.github.com>
2021-01-12 15:30:23 -0600

    [MVS-561] add pdf library

--- PatientRegistrationApp/src/components/HouseholdFollowUp.vue
+++ PatientRegistrationApp/src/components/HouseholdFollowUp.vue
@@ -4,8 +4,8 @@
       <v-col cols="12">
       <div class="font-weight-regular">Use these QR codes to easily check-in at the site where you receive your vaccine. This QR code contains an encrypted patient identifier so we can quickly and securely identify you and retrieve your information.</div></v-col></v-row>
 		<v-row justify="center">
-			<v-btn color="secondary" class="ma-2 white--text">
-				Download All
+			<v-btn color="secondary" class="ma-2 white--text" @click="generatePdf">
+				Download As PDF
 			</v-btn>
 		</v-row>
 		<v-row justify="center">	
@@ -56,6 +56,7 @@
 <script>
 import EventBus from '../eventBus';
 import VueQrcode from 'vue-qrcode';
+import jsPDF from 'jspdf';
 
 	export default {
 	data () {
@@ -70,6 +71,14 @@ import VueQrcode from 'vue-qrcode';
 	},
 	methods:
 	{
+		generatePdf() {
+			let pdfDoc = new jsPDF();
+			pdfDoc.setFontSize(10);
+			pdfDoc.text("COVID-19 Vaccination Registration",10,15);
+			pdfDoc.text("Use this QR code to easily check-in at the site where you receive your vaccine.",10,25);
+			pdfDoc.text("This QR code contains an encrypted patient identifier so we can quickly and securely identify you and retrieve your information.",10, 30);
+			pdfDoc.save('COVID-19_Registration_QR_Code.pdf');
+		},
 		updateHouseholdPersonalInfoData(householdPersonalInfoPayload, householdMemberNumber) {
 			// if member is next in line, add to array of members
 			if (householdMemberNumber-1 == this.dataHouseholdPersonalInfo.length) {
