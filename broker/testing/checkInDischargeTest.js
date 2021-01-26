d92934a7f430e2cac8c3e72cc56594baa0986e48
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2021-01-25 23:55:50 -0500

    Update discharge test

--- broker/testing/checkInDischargeTest.js
+++ broker/testing/checkInDischargeTest.js
@@ -29,9 +29,13 @@ async function cleanup() {
 /**
  * Send request to discharge endpoint.
  */
-async function discharge() {
+async function discharge(appointment, encounter) {
   await globals.broker
-    .post("/discharge", {}, { params: { appointment: "example" } })
+    .post(
+      "/discharge",
+      {},
+      { params: { appointment: appointment, encounter: encounter } }
+    )
     .then((response) => {
       globals.config(response);
       console.log(JSON.stringify(response.data));
@@ -44,12 +48,16 @@ async function discharge() {
  * Send request to check-in endpoint.
  */
 async function checkIn() {
-  await globals.broker
+  return await globals.broker
     .post("/check-in", {}, { params: { patient: "example" } })
     .then((response) => {
       globals.config(response);
       console.log(JSON.stringify(response.data));
       console.log("\n");
+
+      let appointment = response.data.Appointment.id;
+      let encounter = response.data.Encounter.id;
+      return { appointment: appointment, encounter: encounter };
     })
     .catch((error) => globals.info(error));
 }
@@ -58,8 +66,9 @@ async function checkIn() {
  * Check-in and then discharge.
  */
 async function test() {
-  await checkIn();
-  await discharge();
+  let response = await checkIn();
+
+  await discharge(response.appointment, response.encounter);
 }
 
 module.exports = async () => {
