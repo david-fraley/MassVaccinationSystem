e862924d4bc6f47479111ddf454c98ac356e335c
Nick Collins <nick@appomni.com>
2021-02-26 09:41:28 -0700

    header fix font, simplify

--- PatientRegistrationApp/src/pages/application/partials/Header.vue
+++ PatientRegistrationApp/src/pages/application/partials/Header.vue
@@ -1,22 +1,12 @@
 <template>
   <v-toolbar flat height="80em" color="primary" dark>
-    <!--font size for xs & sm screens-->
-    <div class="d-sm-none">
+    <div>
       <v-toolbar-title
-        style="font-size:1.4rem"
         class="font-weight-bold text-wrap"
+        :class="[$vuetify.breakpoint.xs?'font-xs':'font-sm']"
         >{{ title }}</v-toolbar-title
       >
     </div>
-    <!--font size for screens larger than sm-->
-    <div class="d-none d-sm-block">
-      <v-toolbar-title
-        style="font-size:2rem"
-        class="font-weight-bold text-wrap"
-        >{{ title }}</v-toolbar-title
-      >
-    </div>
-
     <v-spacer></v-spacer>
     <v-img max-height="5em" max-width="5em" src="@/assets/Logo.png"></v-img>
   </v-toolbar>
@@ -32,7 +22,11 @@ export default {
 };
 </script>
 <style lang="css" scoped>
-.v-toolbar__title {
-  font-size: 1.3rem;
+.font-sm {
+  font-size: 2rem
+}
+.font-xs {
+  font-size: 1rem
 }
 </style>
+'roomPadding': $vuetify.breakpoint.xs
