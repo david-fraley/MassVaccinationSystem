546099cec212612fb5951de57614c44e7989e4b3
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2021-03-24 11:33:26 -0500

    Update ApplicationInfoPage.vue

--- PointOfDispensingApp/src/components/ApplicationInfoPage.vue
+++ PointOfDispensingApp/src/components/ApplicationInfoPage.vue
@@ -18,21 +18,21 @@
           <v-icon x-large color="#E1F5FE">mdi-square</v-icon>
         </v-col>
         <v-col cols="10">
-          <div class="secondary--text">Patient has not received any doses of the vaccine</div>
+          <div class="secondary--text">Patient has not received any doses of the vaccine / patient is here to receive the first dose.</div>
         </v-col>
         <v-col cols="2">
           <v-icon x-large color="#FF8F00">mdi-square</v-icon>
           <v-icon x-large color="#FFF8E1">mdi-square</v-icon>
         </v-col>
         <v-col cols="10">
-          <div class="secondary--text">Patient has received one (1) dose of a vaccine</div>
+          <div class="secondary--text">Patient has received one (1) dose of a vaccine / patient is here to receive the second dose.</div>
         </v-col>
         <v-col cols="2">
           <v-icon x-large color="#C2185B">mdi-square</v-icon>
           <v-icon x-large color="#FCE4EC">mdi-square</v-icon>
         </v-col>
         <v-col cols="10">
-          <div class="secondary--text">Patient has received two (2) or more doses of the vaccine</div>
+          <div class="secondary--text">Patient has received two (2) or more doses of the vaccine / patient is here to receive booster dose.</div>
         </v-col>
       </v-row>
     </v-container>
