bf826dc0167975013054fda135fb73efbae7707f
dmcook5208 <61951196+dmcook5208@users.noreply.github.com>
2021-03-29 19:40:23 -0500

    Manual merge

--- PointOfDispensingApp/src/components/PatientCheckInPage.vue
+++ PointOfDispensingApp/src/components/PatientCheckInPage.vue
@@ -1,21 +1,26 @@
 <template>
-  <v-container>
-    <v-row>
-      <PatientInfoComponent v-if="!editable"/>
-      <EditPatientComponent v-if="editable"/>
-      <v-col>
-        <h2 class="font-weight-medium primary--text">Vaccination History</h2>
-      </v-col>
-      <PatientHistoryComponent />
-      <v-col cols="12">
-        <v-divider></v-divider>
-      </v-col>
-      <v-col>
-        <h2 class="font-weight-medium primary--text">Check-in</h2>
-      </v-col>
-      <PatientCheckInComponent />
-    </v-row>
-  </v-container>
+  <v-card color="pageColor" elevation="0" tile>
+    <v-toolbar color="primary">
+      <v-toolbar-title class="font-weight-medium white--text">Check-In</v-toolbar-title>
+    </v-toolbar>
+    <v-container>
+      <v-row>
+        <PatientInfoComponent v-if="!editable"/>
+        <EditPatientComponent v-if="editable"/>
+        <v-col>
+          <h2 class="font-weight-medium primary--text">Vaccination History</h2>
+        </v-col>
+        <PatientHistoryComponent />
+        <v-col cols="12">
+          <v-divider></v-divider>
+        </v-col>
+        <v-col>
+          <h2 class="font-weight-medium primary--text">Check-in</h2>
+        </v-col>
+        <PatientCheckInComponent />
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
