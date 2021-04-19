a2764658e01a42833b71aa9dbb0fe1dac0d29c0f
Evan Kleiner <gkleinereva@gmail.com>
2021-04-19 14:21:49 -0700

    CORS whitelisting added to broker (#250)
    * CORS whitelisting added to broker
    
    * Quick doc update
    
    * Update to address no Origin header

--- broker/config/server.js
+++ broker/config/server.js
@@ -1,13 +1,33 @@
 // Read private configuration settings from .env file into process.env
 //
 require("dotenv").config({ path: `${__dirname}/../../.env` });
+const cors = require('cors');
 
 // Export configuration settings with some values set to defaults.
 // NOTE: login/password settings should not be defaulted.
 //
+let allowedCorsOrigins = null;
+if(process.env.CORS_ORIGINS) {
+  allowedCorsOrigins = process.env.CORS_ORIGINS.split(',');
+}
+
 module.exports = {
   port: process.env.PORT || 3000,
   fhirUrlBase:
     process.env.FHIR_URL_BASE || "http://hapi:8080/hapi-fhir-jpaserver/fhir",
-  mirthUrl: process.env.MIRTH_URL || "http://localhost:8444/"
+  mirthUrl: process.env.MIRTH_URL || "http://localhost:8444/",
+
+  cors: cors({
+    origin: function(origin, callback) {
+      
+      // If CORS_ORIGINS specified in .env, make sure origin matches one of them
+      if(!origin || !allowedCorsOrigins || allowedCorsOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      }
+      else {
+        callback(new Error('Request origin blocked by CORS'));
+      }
+    }
+  })
+
 };
